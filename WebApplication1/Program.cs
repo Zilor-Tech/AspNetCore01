03466cc9d2c2eac133b0b12ae975431dbe826a70
Zilor <32511003@qq.com>
2020-04-29 13:12:22 +0800

    Revert "2020年4月29日"
    This reverts commit 5d6fa03c342d40e46fe162381dee0216f614004e.

--- WebApplication1/Program.cs
+++ WebApplication1/Program.cs
@@ -19,6 +19,10 @@ namespace WebApplication1
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
             Host.CreateDefaultBuilder(args)
+
+                // 这句话允许在Host构建时使用强类型的ContainerBuilder方法，以支持Autofac的ConfigureContainer。
+                // 这也会自动调用Populate方法，将我们在ConfigureServices期间注册的服务放到Autofac中。
+                .UseServiceProviderFactory(new AutofacServiceProviderFactory())
                 .ConfigureWebHostDefaults(webBuilder =>
                 {
                     webBuilder.UseStartup<Startup>();
